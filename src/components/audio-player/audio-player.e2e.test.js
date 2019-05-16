c43f2f6f63d403b59e1a96eebdc86d48a1a69014
Daria Kliukhina <dariasergeevna90@gmail.com>
2019-05-16 22:04:34 +0300

    Добавляет е2е

--- src/components/audio-player/audio-player.e2e.test.js
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -1,14 +1,32 @@
-// import React from 'react';
-// import Enzyme, {shallow} from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import AudioPlayer from '../audio-player/audio-player';
-//
-// window.HTMLMediaElement.prototype.play = () => {};
-// window.HTMLMediaElement.prototype.pause = () => {};
-//
-// Enzyme.configure({adapter: new Adapter()});
-//
-// const mock = {
-//   src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-//   clickHandler: jest.fn()
-// };
+import Enzyme, {mount} from 'enzyme';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import AudioPlayer from './audio-player';
+
+Enzyme.configure({adapter: new Adapter()});
+
+const src = `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`;
+
+describe(`<AudioPlayer/>`, () => {
+  it(`shows play icon when it's not playing`, () => {
+    const audioPlayer = mount(<AudioPlayer
+      src={src}
+      onPlayButtonClick={jest.fn()}
+      isPlaying={false}
+    />);
+    const playButton = audioPlayer.find(`button.track__button`);
+
+    expect(playButton.hasClass(`track__button--play`)).toEqual(true);
+  });
+
+  it(`shows pause icon when it's playing`, () => {
+    const audioPlayer = mount(<AudioPlayer
+      src={src}
+      onPlayButtonClick={jest.fn()}
+      isPlaying={true}
+    />);
+    const playButton = audioPlayer.find(`button.track__button`);
+
+    expect(playButton.hasClass(`track__button--pause`)).toEqual(true);
+  });
+});
